237d59eb1a05d2f6509910b4961f2106dc6fd962
jaimelias <escribeme@jaimelias.com>
2020-11-11 21:51:10 -0500

    added gitupdater tag to main fail

--- dynamicpackages.php
+++ dynamicpackages.php
@@ -23,6 +23,8 @@
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       dynamicpackages
  * Domain Path:       /languages
+ * GitHub Plugin URI:       jaimelias/dynamicpackages
+ * GitHub Plugin URI:       https://github.com/jaimelias/dynamicpackages
  */
 
 // If this file is called directly, abort.
