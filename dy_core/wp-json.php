fd4f03647b42fbcd84d5c7658a9e4b2682aa24b6
jaimelias <escribeme@jaimelias.com>
2022-11-13 10:17:18 -0500

    ventas

--- dy_core/wp-json.php
+++ dy_core/wp-json.php
@@ -14,17 +14,19 @@ class Dynamic_Core_WP_JSON {
 
     public function core_args()
     {
-        register_rest_route( 'dy-core', '/args/', array(
+        register_rest_route( 'dy-core', 'args', array(
             'methods' => 'GET',
-            'callback' => array(&$this, 'core_args_callback')
+            'callback' => array(&$this, 'core_args_callback'),
+            'permission_callback' => '__return_true'
         ));
     }
 
     public function core_args_callback($req)
     {
+        $utc_time = date('Y-m-d H:i:s', time());
         $dy_nonce = wp_create_nonce('dy_nonce');
-
-        return array('dy_nonce' => $dy_nonce);
+        
+        return array('dy_nonce' => $dy_nonce, 'utc_date_time' => $utc_time);
     }
 }
 
