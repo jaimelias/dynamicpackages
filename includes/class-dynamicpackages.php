06c66a7586c8e9dbad413af8db91c8e29a293541
Jaime Castillo <escribeme@jaimelias.com>
2025-04-23 14:19:14 -0500

    updated version to 1.2.5

--- includes/class-dynamicpackages.php
+++ includes/class-dynamicpackages.php
@@ -37,7 +37,7 @@ class dynamicpackages {
 
 		$this->plugin_name = 'Dynamic Packages';
 		$this->plugin_id = 'dynamicpackages';
-		$this->version = '1.2.4';
+		$this->version = '1.2.5';
 		
 		$this->load_dependencies();
 
