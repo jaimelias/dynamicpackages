2c62f013568ada1fb639fa540082d6669df68f19
Jaime Castillo <escribeme@jaimelias.com>
2025-09-15 13:59:07 -0500

    removed package_redirect_url language based specific excludes

--- includes/class-dynamicpackages-fields.php
+++ includes/class-dynamicpackages-fields.php
@@ -68,7 +68,6 @@ class Dynamicpackages_Fields
             'package_discount',
             'package_increase_persons',
             'package_disabled_dates_api',
-            'package_redirect_url'
         ];
 
         // Add day-specific excludes
