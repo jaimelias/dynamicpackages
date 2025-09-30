673b623033184058f240b255ff2be913534ef1d3
Jaime Castillo <escribeme@jaimelias.com>
2025-09-30 13:33:05 -0500

    added cloudflare to loader

--- dy-core/loader.php
+++ dy-core/loader.php
@@ -16,6 +16,7 @@ if(!class_exists('Dy_Core_Init'))
         public function load_dependencies()
         {
             require_once $this->plugin_dir_path . 'waf.php';
+            require_once $this->plugin_dir_path . 'cloudflare.php';
             require_once $this->plugin_dir_path . 'sitemap.php';
             require_once $this->plugin_dir_path . 'queries.php';
             require_once $this->plugin_dir_path . 'training-data/concatenate_object_to_text.php';
