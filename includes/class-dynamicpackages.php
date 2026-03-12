0aadb3f7dbca51c9f35daff01bd66a254f9186a6
Jaime Castillo <escribeme@jaimelias.com>
2026-03-11 22:20:50 -0500

    added url to log error message in getNonce

--- includes/class-dynamicpackages.php
+++ includes/class-dynamicpackages.php
@@ -37,7 +37,7 @@ class dynamicpackages {
 
 		$this->plugin_name = 'Dynamic Packages';
 		$this->plugin_id = 'dynamicpackages';
-		$this->version = '1.3.5';
+		$this->version = '1.3.6';
 		
 		$this->load_dependencies();
 
