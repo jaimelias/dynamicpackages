d09a32934be0db2dfc03c8d30224af24b4f17d6a
Jaime Castillo <escribeme@jaimelias.com>
2026-06-26 17:03:44 -0500

    removed xml to r2

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInitefa4f9d2242590a5d7b1dda344648502
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInitefa4f9d2242590a5d7b1dda344648502', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInitefa4f9d2242590a5d7b1dda344648502', 'loadClassLoader'));
